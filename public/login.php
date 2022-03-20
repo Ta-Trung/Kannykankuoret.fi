3930c1bb396aa90348a846c83e6faa2bdfc89b4b
Trung Ta <57113885+Ta-Trung@users.noreply.github.com>
2022-03-20 21:57:11 +0200

    implemeted login page

--- public/login.php
+++ public/login.php
@@ -4,21 +4,22 @@
 <?php include(TEMPLATE_FRONT . DS . "header.php") ?>
 
 <!-- LOGIN FORM -->
-<div class="container form-container">
+<div class="container form-container d-block text-center">
+    <div><h2 class="text-danger"><?php display_message(); ?></h2></div>
     <form action="" method="post" class="form-signin text-center">
         <?php login_user(); ?>
         <img src="./assets/img/KännykänKuoretLogo.png" alt="" class="mb-4" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label for="inputUsername" class="sr-only">Username</label>
-        <input type="text" class="form-signin__control" id="inputUsername" placeholder="User name" required="" autofocus="">
+        <input type="text" name="username" class="form-signin__control" id="inputUsername" placeholder="User name" required="" autofocus="">
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" class="form-signin__control" id="inputPassword" placeholder="Password" required="">
+        <input type="password" name="password" class="form-signin__control" id="inputPassword" placeholder="Password" required="">
         <div class="form-signin__checkbox mb-3">
             <label>
                 <input type="checkbox" value="remember-me"> Remember me
             </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Sign in</button>
         <p class="mt-5 mb-3 text-muted">© Trung Ta 2022</p>
     </form>
 </div>
--- resources/functions.php
+++ resources/functions.php
@@ -1,6 +1,22 @@
 <?php
 
 //HELPER FUNCTIONS
+
+function set_message($msg){
+  if(!empty($msg)){
+    $_SESSION['message'] = $msg;
+  } else{
+    $msg = "";
+  }
+}
+
+function display_message(){
+  if(isset($_SESSION['message'])){
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+  }
+}
+
 function redirect($location){
     header("Location: $location ");
 }
@@ -168,18 +184,21 @@ function get_products_in_shop_page(){
   }
 }
 
+//LOGIN SYSTEM
 function login_user(){
   if(isset($_POST['submit'])){
     $username = escape_string($_POST['username']);
     $password = escape_string($_POST['password']);
 
-    $query = query("SELECT * FROM users WHERE usernam= '{$username}' AND password = '{$password}'");
+    $query = query("SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}' ");
     confirm($query);
 
     if(mysqli_num_rows($query) == 0){
+
+      set_message("Your password or username are incorrect");
       redirect("login.php");
     }else{
-      redirect("admin");
+      redirect("./admin/admin.html");
     }
   }
 }
