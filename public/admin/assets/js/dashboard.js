45fabb778b925464d636df92d6c97568b0ed0505
Trung Ta <57113885+Ta-Trung@users.noreply.github.com>
2022-03-20 21:13:15 +0200

    implemented admin page with chart

--- public/admin/assets/js/dashboard.js
+++ public/admin/assets/js/dashboard.js
@@ -3,20 +3,59 @@
 // You have two choices for including Bootstrap's JS files—the whole thing,
 // or just the bits that you need.
 
-
 // Option 1
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
+//import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // Option 2
 //
 // Import just what we need
 
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
+import "../../node_modules/popper.js/dist/popper.min.js";
 import "../../node_modules/bootstrap/js/dist/util.js";
 import "../../node_modules/bootstrap/js/dist/modal.js";
+import "../../node_modules/chart.js/dist/chart.js";
+
+//CHART
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+    datasets: [
+      {
+        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+        lineTension: 0,
+        backgroundColor: "transparent",
+        borderColor: "#007bff",
+        borderWidth: 4,
+        pointBackgroundColor: "#007bff",
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false,
+          },
+        },
+      ],
+    },
+    legend: {
+      display: false,
+    },
+  },
+});
