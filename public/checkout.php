f91961a0b3eb437f8d9da3162391037c760794c2
Trung Ta <57113885+Ta-Trung@users.noreply.github.com>
2022-03-22 21:58:56 +0200

    start to implement shopping cart function

---
+++ public/checkout.php
@@ -0,0 +1,9 @@
+<!-- GET GLOBAL CONNECTION FROM CONFIG.PHP -->
+<?php require_once("../resources/config.php") ?>
+
+<!-- HEADER -->
+<?php include(TEMPLATE_FRONT .DS. "header.php") ?>
+
+
+<!-- FOOTER -->
+<?php include(TEMPLATE_FRONT .DS. "footer.php") ?>
