6d15a0015b95ea5af05a03d612b407bc5926c77a
Trung Ta <57113885+Ta-Trung@users.noreply.github.com>
2022-03-30 21:54:00 +0300

    added css to edit-product page

--- resources/template/back/edit_product.php
+++ resources/template/back/edit_product.php
@@ -49,6 +49,147 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="" class="form-label">Price</label>
+                                        <div class="input-group mb-3">
+                                           <div class="input-group-prepend">
+                                               <span class="input-group-text" id="basic-addon1">
+                                                <svg class="bi text-dark" width="24" height="24">
+                                                    <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#cash"/>
+                                                </svg>
+                                               </span>
+                                           </div>
+                                           <input type="text" class="form-control" placeholder="price" aria-label="price" aria-describedby="basic-addon1"> 
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="" class="form-label">Discount</label>
+                                        <div class="input-group mb-3">
+                                           <div class="input-group-prepend">
+                                               <span class="input-group-text" id="basic-addon2">
+                                                <svg class="bi text-dark" width="24" height="24">
+                                                    <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#scissors"/>
+                                                </svg>
+                                               </span>
+                                           </div>
+                                           <input type="text" class="form-control" placeholder="discount" aria-label="discount" aria-describedby="basic-addon2"> 
+                                        </div>
+                                    </div>
+                                </div>
+                                <h5 class="card-title m-t-40">Product Description</h5>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <textarea class="form-control" rows="4">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt 
+                                            amet autem harum molestiae cumque cupiditate cum nam voluptate nobis eum?
+                                        </textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="" class="form-label">Meta title</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="" class="form-label">Meta Keywork</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <h5 class="card-title m-t-20">Upload Image</h5>
+                                    <div class="product-img">
+                                        <img src="https://via.placeholder.com/200x249" alt="">
+                                        <div class="pro-img-overlay">
+                                            <a href="#" class="bg-info">
+                                                <svg class="bi text-dark" width="24" height="24">
+                                                    <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                                </svg>
+                                            </a>
+                                            <a href="#" class="bg-danger">
+                                                <svg class="bi text-dark" width="24" height="24">
+                                                    <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                                </svg>
+                                            </a>
+                                            <br>
+                                        </div>
+                                        <div class="fileupload btn btn-info wave-effect-wave-light text-white">
+                                            <span>Upload Anonther Image</span>
+                                            <input type="file" class="upload">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h5 class="card-title mt-5">General Info</h5>
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered td-padding">
+                                            <tbody>
+                                                <tr>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Brand">
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Stellar">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Brand">
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Stellar">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Brand">
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Stellar">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Brand">
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Stellar">
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Brand">
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control" placeholder="Stellar">
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="form-action mt-5">
+                                <button type="submit" class="btn btn-success text-white">
+                                    <svg class="bi" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#check"/>
+                                    </svg>
+                                    Save
+                                </button>
+                                <button type="button" class="btn btn-dark"> Cancel</button>
+                            </div>
                         </div>
                     </form>
                 </div>
