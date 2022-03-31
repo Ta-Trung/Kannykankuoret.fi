5c349244692d4dbc90e569c4d247af176de1361f
Trung Ta <57113885+Ta-Trung@users.noreply.github.com>
2022-03-31 20:49:38 +0300

    added css and html to admin pages

---
+++ resources/template/back/categories.php
@@ -0,0 +1,220 @@
+<?php include(TEMPLATE_BACK . "/sidebar_nav.php"); ?>
+<section class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4 product-admin">
+    <div class="container-fluid">
+        <div class="row product-admin__page-title">
+            <div class="col-md-5 align-self-center">
+                <h4 class="product-admin__text-themecolor">Categories</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="product-img">
+                            <img src="https://via.placeholder.com/335x249" alt="">
+                            <div class="pro-img-overlay">
+                                <a href="#" class="bg-info">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#pencil"/>
+                                    </svg>
+                                </a>
+                                <a href="#" class="bg-danger">
+                                    <svg class="bi text-dark" width="24" height="24">
+                                        <use xlink:href="node_modules/bootstrap-icons/bootstrap-icons.svg#trash"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <span class="pro-price bg-primary text-dark">€15</span>
+                            <h5 class="card-title mb-0">Product title</h5>
+                            <small class="text-muted db">Product description small</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
